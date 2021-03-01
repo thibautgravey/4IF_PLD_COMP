2476555a5f5126e6f1e37a63e1c797225742e631
koechslin <killian.oechslin@insa-lyon.fr>
2021-03-01 10:15:03 +0100

    feat: add prologue and epilogue

--- compiler/visitor.h
+++ compiler/visitor.h
@@ -24,7 +24,15 @@ public:
      int retval = stoi(ctx->CONST()->getText());
      std::cout<<".globl	main\n"
            " main: \n"
+           "  # prologue\n"
+           "  pushq %rbp\n"
+           "  movq %rsp, %rbp\n"
+           "\n"
+           "  # body\n"
            " 	movl	$"<<retval<<", %eax\n"
+           "\n"
+           "  # epilogue\n"
+           "  popq %rbp\n"
            " 	ret\n";
 
      return 0;
