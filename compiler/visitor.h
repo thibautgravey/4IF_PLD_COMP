320d19ac45a0f83c0e1c998430b09bdf1b654b77
tmignot63 <gotamt99@gmail.com>
2021-03-06 11:31:55 +0100

    feat : first (stupid) integration of multiplication Only  var * const and const*const for the moment

--- compiler/visitor.h
+++ compiler/visitor.h
@@ -51,18 +51,22 @@ public:
     // calculate space needed for variables
     int spaceNeeded = 0;
     map<string, varInfo>::iterator it;
-    for(it = variables.begin(); it != variables.end(); it++) {
-      if(it->second.offset != nullptr) {
+    for (it = variables.begin(); it != variables.end(); it++)
+    {
+      if (it->second.offset != nullptr)
+      {
         // if offset is not null, it means the variable need space
         spaceNeeded += 4; // 4 for INT
       }
-      else {
+      else
+      {
         cout << "[Warning] La variable '" << it->first << "' a été déclarée mais n'est pas utilisée." << endl;
       }
     }
 
     // round space to the nearest multiple of 16
-    if(spaceNeeded % 16) {
+    if (spaceNeeded % 16)
+    {
       spaceNeeded = ((spaceNeeded / 16) + 1) * 16;
     }
 
@@ -72,19 +76,26 @@ public:
 
     exprInfo retExprInfo = visit(ctx->expr());
 
-    if(retExprInfo.isConst) {
-      assembly += "  movl $" + to_string(*((int *) retExprInfo.value)) + ", %eax\n";
-    } else {
+    if (retExprInfo.isConst)
+    {
+      assembly += "  movl $" + to_string(*((int *)retExprInfo.value)) + ", %eax\n";
+    }
+    else
+    {
       map<string, varInfo>::iterator it = variables.find(retExprInfo.varExprName);
-      if(it != variables.end()) {
-        if(it->second.offset == nullptr) {
+      if (it != variables.end())
+      {
+        if (it->second.offset == nullptr)
+        {
           //cout << "[visitProg] Erreur la variable '" << retExprInfo.varExprName << "' n'a pas de valeur !" << endl;
           variablesOffset -= 4; // 4 pour INT
           it->second.offset = new int;
           *(it->second.offset) = variablesOffset;
         }
-        assembly += "  movl " + to_string(*((int *) it->second.offset)) + "(%rbp), %eax\n";
-      } else {
+        assembly += "  movl " + to_string(*((int *)it->second.offset)) + "(%rbp), %eax\n";
+      }
+      else
+      {
         hasError = true;
         cout << "[visitProg] Erreur la variable '" << retExprInfo.varExprName << "' n'a pas été déclarée !" << endl;
         assembly += "  movl $-1, %eax\n";
@@ -94,13 +105,14 @@ public:
     // add epilogue
     assembly += "\n"
                 "  # epilogue\n"
-                "  addq $" + to_string(spaceNeeded) + ", %rsp\n"
-                "  popq %rbp\n"
-                "  ret\n";
-
+                "  addq $" +
+                to_string(spaceNeeded) + ", %rsp\n"
+                                         "  popq %rbp\n"
+                                         "  ret\n";
 
     // write the assembly if there is no error
-    if (!hasError) cout << assembly ;
+    if (!hasError)
+      cout << assembly;
 
     /*
     map<string, varInfo>::iterator it;
@@ -148,26 +160,33 @@ public:
     {
       exprInfo retExprInfo = visit(ctx->expr());
 
-      if(retExprInfo.isConst) {
+      if (retExprInfo.isConst)
+      {
         val = new int;
-        *val = *((int *) retExprInfo.value);
+        *val = *((int *)retExprInfo.value);
         variablesOffset -= 4; // 4 pour INT
         offset = new int;
         *offset = variablesOffset;
         assemblyTmpBuf += "  movl $" + to_string((*val)) + ", " + to_string(*offset) + "(%rbp)\n";
-      } else {
+      }
+      else
+      {
         map<string, varInfo>::iterator it = variables.find(retExprInfo.varExprName);
-        if(it != variables.end()) {
-          if(it->second.val != nullptr) {
+        if (it != variables.end())
+        {
+          if (it->second.val != nullptr)
+          {
             val = new int;
             *val = *(it->second.val);
           }
-          variablesOffset -=4;
+          variablesOffset -= 4;
           offset = new int;
           *offset = variablesOffset;
           assemblyTmpBuf += "  movl " + to_string(*(it->second.offset)) + "(%rbp), %eax\n";
           assemblyTmpBuf += "  movl %eax, " + to_string(*offset) + "(%rbp)\n";
-        } else {
+        }
+        else
+        {
           hasError = true;
           cout << "[visitVar_decl] Erreur affectation de la variable '" << retExprInfo.varExprName << "' qui n'a pas été déclarée !" << endl;
         }
@@ -190,17 +209,22 @@ public:
     {
       exprInfo retExprInfo = visit(ctx->expr());
 
-      if(retExprInfo.isConst) {
+      if (retExprInfo.isConst)
+      {
         it->second.val = new int;
-        *(it->second.val) = *((int *) retExprInfo.value);
+        *(it->second.val) = *((int *)retExprInfo.value);
         variablesOffset -= 4; // 4 pour INT
         it->second.offset = new int;
         *(it->second.offset) = variablesOffset;
         assemblyTmpBuf += "  movl $" + to_string(*(it->second.val)) + ", " + to_string(*(it->second.offset)) + "(%rbp)\n";
-      } else {
+      }
+      else
+      {
         map<string, varInfo>::iterator it2 = variables.find(retExprInfo.varExprName);
-        if(it2 != variables.end()) {
-          if(it2->second.val != nullptr) {
+        if (it2 != variables.end())
+        {
+          if (it2->second.val != nullptr)
+          {
             it->second.val = new int;
             *(it->second.val) = *(it2->second.val);
           }
@@ -209,7 +233,9 @@ public:
           *(it->second.offset) = variablesOffset;
           assemblyTmpBuf += "  movl " + to_string(*(it2->second.offset)) + "(%rbp), %eax\n";
           assemblyTmpBuf += "  movl %eax, " + to_string(*(it->second.offset)) + "(%rbp)\n";
-        } else {
+        }
+        else
+        {
           hasError = true;
           cout << "[visitVar_aff] Erreur affectation de la variable '" << retExprInfo.varExprName << "' qui n'a pas été déclarée !" << endl;
         }
@@ -246,7 +272,79 @@ public:
 
   virtual antlrcpp::Any visitMult(ifccParser::MultContext *ctx) override
   {
-    return (int)visit(ctx->expr(0)) * (int)visit(ctx->expr(1));
+    exprInfo ret0 = visit(ctx->expr(0));
+    exprInfo ret1 = visit(ctx->expr(1));
+
+    exprInfo ret;
+    exprInfo retCopie;
+
+    ret.isConst = true;
+    ret.value = new int;
+    *((int *)ret.value) = (*((int *)ret0.value)) * (*((int *)ret1.value));
+
+    map<string, varInfo>::iterator it;
+
+    if ((ret0.isConst && !ret1.isConst) || (!ret0.isConst && ret1.isConst))
+    {
+      int constValue;
+      if (ret0.isConst && !ret1.isConst)
+      {
+        it = variables.find(ret1.varExprName);
+        retCopie = ret1;
+        constValue = (*((int *)ret0.value));
+      }
+
+      else
+      {
+        it = variables.find(ret0.varExprName);
+        retCopie = ret0;
+        constValue = (*((int *)ret1.value));
+      }
+
+      if (it != variables.end())
+      {
+
+        //à quoi sert cette vérif ?
+        assemblyTmpBuf += "  movl " + to_string(*(it->second.offset)) + "(%rbp), %eax\n";
+        assemblyTmpBuf += "  imul $" + to_string(constValue) + ", %eax\n";
+      }
+      else
+      {
+        hasError = true;
+        cout << "[visitMult] Erreur multiplication de la variable '" << retCopie.varExprName << "' qui n'a pas été déclarée !" << endl;
+      }
+    }
+
+    /*
+    if (!ret0.isConst && !ret1.isConst)
+    {
+
+      it = variables.find(ret0.varExprName);
+      map<string, varInfo>::iterator it2 = variables.find(ret1.varExprName);
+
+      if (it != variables.end() && it2 != variables.end())
+      {
+        if (it->second.val != nullptr && it2->second.val != nullptr)
+        {
+          //à quoi sert cette vérif ?
+          assemblyTmpBuf += "  movl " + to_string(*(it->second.offset)) + "(%rbp), %eax\n";
+          assemblyTmpBuf += "  imul $" + to_string(ret0.value) + ", %eax\n";
+        }
+      }
+
+      else
+      {
+        hasError = true;
+        if (it == variables.end())
+        {
+          cout << "[visitMult] Erreur multiplication de la variable '" << retExprInfo.varExprName << "' qui n'a pas été déclarée !" << endl;
+        }
+
+        //2 variables
+      }
+    }
+    */
+    return ret;
   }
 
   virtual antlrcpp::Any visitConst(ifccParser::ConstContext *ctx) override
@@ -269,12 +367,13 @@ public:
     map<string, varInfo>::iterator it = variables.find(varName);
     if (it != variables.end())
     {
-      if(it->second.val != nullptr)
+      if (it->second.val != nullptr)
       {
         ret.value = new int;
         *((int *)ret.value) = *(it->second.val);
       }
-      if(it->second.offset == nullptr) {
+      if (it->second.offset == nullptr)
+      {
         variablesOffset -= 4; // 4 pour INT
         it->second.offset = new int;
         *(it->second.offset) = variablesOffset;
