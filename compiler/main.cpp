850706505afba8941f5e84fa6005f9d8cdd99833
thibautgravey <thibaut.gravey@insa-lyon.fr>
2021-03-07 15:17:46 +0100

    feat: structural skeleton in main

--- compiler/main.cpp
+++ compiler/main.cpp
@@ -13,30 +13,45 @@ using namespace antlr4;
 using namespace std;
 
 int main(int argn, const char **argv) {
-  stringstream in;
-  if (argn==2) {
-     ifstream lecture(argv[1]);
-     in << lecture.rdbuf();
-  }
-  ANTLRInputStream input(in.str());
-  ifccLexer lexer(&input);
-  CommonTokenStream tokens(&lexer);
+    stringstream in;
+    if (argn>=2) {
+        ifstream lecture(argv[1]);
+        in << lecture.rdbuf();
+    }
+    ANTLRInputStream input(in.str());
+    ifccLexer lexer(&input);
+    CommonTokenStream tokens(&lexer);
 
-  tokens.fill();
+    tokens.fill();
 //  for (auto token : tokens.getTokens()) {
 //    std::cout << token->toString() << std::endl;
 //  }
 
-  ifccParser parser(&tokens);
-  tree::ParseTree* tree = parser.axiom();
+    ifccParser parser(&tokens);
+    tree::ParseTree* tree = parser.axiom();
 
-  Visitor visitor;
-  bool visitorError = visitor.visit(tree);
+    //TODO : Create a real ASTVisitor/ASTGenerator and generate an AST
 
-  int lexerErrors = lexer.getNumberOfSyntaxErrors();
-  int parserErrors = parser.getNumberOfSyntaxErrors();
+    Visitor visitor;
+    bool visitorError = visitor.visit(tree);
 
-  if(lexerErrors || parserErrors || visitorError) return 1;
+    int lexerErrors = lexer.getNumberOfSyntaxErrors();
+    int parserErrors = parser.getNumberOfSyntaxErrors();
 
-  return 0;
+    if(lexerErrors || parserErrors || visitorError) return 1;
+
+
+    //TODO : Create an IR and generate ASM
+
+    //Create the output path
+    string outPath(argv[1]);
+    if(argn == 3) {
+        outPath = string(argv[2]);
+    }
+    outPath.erase(outPath.find("."));
+    outPath.append(".s");
+
+    //TODO : Generate an exec file with a an "as file.s -o file.o" and then link using gcc
+
+    return 0;
 }
