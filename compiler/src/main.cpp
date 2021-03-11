6968c98c02c3401fa26a0c54ba8827960c4e4536
thibautgravey <thibaut.gravey@insa-lyon.fr>
2021-03-11 16:33:49 +0100

    feat: write ASM into a .s file depending on 2nd args

--- compiler/src/main.cpp
+++ compiler/src/main.cpp
@@ -41,15 +41,30 @@ int main(int argn, const char ** argv) {
         return 1;
 
     //TODO : Create an IR and generate ASM
-    cout << program->GenerateAsm();
+    string generatedAsm = program->GenerateAsm();
+    cout << generatedAsm << endl;
 
     //Create the output path
     string outPath(argv[1]);
     if (argn == 3) {
         outPath = string(argv[2]);
     }
-    outPath.erase(outPath.find("."));
+
+    size_t dotPosition = outPath.find('.');
+    if(dotPosition!=string::npos){
+        outPath.erase(outPath.find('.'));
+    }
     outPath.append(".s");
+
+    ofstream out(outPath);
+    if(out.bad()) {
+        cout << "An error occured when writing Generated ASM to "+outPath << endl;
+    } else {
+        out << generatedAsm;
+        cout << "Generated ASM has been written to "+outPath << endl;
+    }
+
+    out.close();
 
     //TODO : Generate an exec file with a an "as file.s -o file.o" and then link using gcc
 
