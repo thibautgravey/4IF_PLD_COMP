4ae41095de3e6d6329c4703fabbd541719513bb2
thibautgravey <thibaut.gravey@insa-lyon.fr>
2021-03-14 00:33:47 +0100

    refactor: clean ASTGenerator skeleton

--- compiler/src/ASTGenerator.cpp
+++ compiler/src/ASTGenerator.cpp
@@ -22,7 +22,119 @@ using namespace std;
 
 //----------------------------------------------------- Méthodes publiques
 
+antlrcpp::Any ASTGenerator::visitAxiom(ifccParser::AxiomContext * ctx) {
+    return visit(ctx->prog());
+} //----- Fin de visitAxiom
+
+antlrcpp::Any ASTGenerator::visitProg(ifccParser::ProgContext * ctx) {
+    program = new Program(ctx->start->getLine());
+    program->GetSymbolTable().DefineFunction("main", INT, ctx->start->getLine());
+
+    for (int i = 0; i < ctx->line().size(); i++) {
+        Instr * instr = (Instr *)visit(ctx->line(i));
+        if (instr != nullptr) {
+            program->AddInstr(instr);
+        }
+    }
+
+    return program;
+} //----- Fin de visitProg
+
+antlrcpp::Any ASTGenerator::visitLine(ifccParser::LineContext * ctx) {
+    Instr * instr;
+    if (ctx->var_aff()) {
+        instr = (Instr *)visit(ctx->var_aff());
+    } else if (ctx->return_stmt()) {
+        instr = (Instr *)visit(ctx->return_stmt());
+    } else if (ctx->var_decl()) {
+        instr = (Instr *)visit(ctx->var_decl());
+    }
+
+    return instr;
+} //----- Fin de visitLine
+
+antlrcpp::Any ASTGenerator::visitVar_decl(ifccParser::Var_declContext * ctx) {
+    Instr * ret = nullptr;
+    if (program->GetSymbolTable().DefineVariable("main", ctx->VAR_NAME()->getText(), INT, ctx->start->getLine())) {
+        if (ctx->expr()) {
+            Expr * expr = (Expr *)visit(ctx->expr());
+            ret = new VarAffInstr(ctx->start->getLine(), ctx->VAR_NAME()->getText(), expr);
+        }
+    } else {
+        program->SetErrorFlag(true);
+    }
+
+    return ret;
+} //----- Fin de visitVar_decl
+
+antlrcpp::Any ASTGenerator::visitVar_aff(ifccParser::Var_affContext * ctx) {
+    Expr * expr = (Expr *)visit(ctx->expr());
+    return (Instr *)new VarAffInstr(ctx->start->getLine(), ctx->VAR_NAME()->getText(), expr);
+} //----- Fin de visitVar_aff
+
+antlrcpp::Any ASTGenerator::visitReturn_stmt(ifccParser::Return_stmtContext * ctx) {
+    Expr * expr = (Expr *)visit(ctx->expr());
+    return (Instr *)new ReturnInstr(ctx->start->getLine(), expr);
+} //----- Fin de visitReturn_stmt
+
+antlrcpp::Any ASTGenerator::visitPar(ifccParser::ParContext * ctx) {
+    return visit(ctx->expr());
+} //----- Fin de visitPar
+
+antlrcpp::Any ASTGenerator::visitAdd(ifccParser::AddContext * ctx) {
+    Expr * op1 = (Expr *)visit(ctx->expr(0));
+    Expr * op2 = (Expr *)visit(ctx->expr(1));
+    BinaryOperator binaryOperatorPlus = PLUS;
+
+    return (Expr *)new OpBin(ctx->start->getLine(), op1, op2, binaryOperatorPlus);
+} //----- Fin de visitAdd
+
+antlrcpp::Any ASTGenerator::visitLess(ifccParser::LessContext * ctx) {
+    Expr * op1 = (Expr *)visit(ctx->expr(0));
+    Expr * op2 = (Expr *)visit(ctx->expr(1));
+    BinaryOperator binaryOperatorMinus = MINUS;
+
+    return (Expr *)new OpBin(ctx->start->getLine(), op1, op2, binaryOperatorMinus);
+} //----- Fin de visitLess
+
+antlrcpp::Any ASTGenerator::visitDiv(ifccParser::DivContext * ctx) {
+    Expr * op1 = (Expr *)visit(ctx->expr(0));
+    Expr * op2 = (Expr *)visit(ctx->expr(1));
+    BinaryOperator binaryOperatorDiv = DIV;
+
+    return (Expr *)new OpBin(ctx->start->getLine(), op1, op2, binaryOperatorDiv);
+} //----- Fin de visitDiv
+
+antlrcpp::Any ASTGenerator::visitMult(ifccParser::MultContext * ctx) {
+    Expr * op1 = (Expr *)visit(ctx->expr(0));
+    Expr * op2 = (Expr *)visit(ctx->expr(1));
+    BinaryOperator binaryOperatorMult = MULT;
+
+    return (Expr *)new OpBin(ctx->start->getLine(), op1, op2, binaryOperatorMult);
+} //----- Fin de visitMult
+
+antlrcpp::Any ASTGenerator::visitConst(ifccParser::ConstContext * ctx) {
+    return (Expr *)new ConstLiteral(ctx->start->getLine(), stoi(ctx->getText()));
+} //----- Fin de visitConst
+
+antlrcpp::Any ASTGenerator::visitVar(ifccParser::VarContext * ctx) {
+    Expr * ret = nullptr;
+    if (program->GetSymbolTable().LookUp("main", ctx->VAR_NAME()->getText())) {
+        ret = new Var(ctx->start->getLine(), ctx->VAR_NAME()->getText());
+        program->GetSymbolTable().SetUsedVariable("main", ctx->VAR_NAME()->getText());
+    } else {
+        program->SetErrorFlag(true);
+        cerr << "variable " + ctx->VAR_NAME()->getText() + " does not exist in contextVariableTable from " + "main";
+    }
+
+    return ret;
+} //----- Fin de visitVar
+
 //-------------------------------------------- Constructeurs - destructeur
+
+ASTGenerator::~ASTGenerator() {
+    delete(program);
+} //----- Fin de ~ASTGenerator
 
 //------------------------------------------------------------------ PRIVE
 
--- compiler/src/ASTGenerator.h
+++ compiler/src/ASTGenerator.h
@@ -1,8 +1,14 @@
+/*******************************************************************************
+                           ASTGenerator  -  description
+                             -------------------
+    copyright            : (C) 2021 par l'HexaOne
+*******************************************************************************/
 
-// Generated from ifcc.g4 by ANTLR 4.7.2
+//-------- Interface de la classe <ASTGenerator> (fichier ASTGenerator.h) ---------
+#if !defined(ASTGENERATOR_H)
+#define ASTGENERATOR_H
 
-#pragma once
-
+//--------------------------------------------------- Interfaces utilisées
 #include "antlr4-runtime.h"
 #include "ifccBaseVisitor.h"
 #include <map>
@@ -11,116 +17,65 @@
 
 using namespace std;
 
+//------------------------------------------------------------- Constantes
+
+//------------------------------------------------------------------ Types
+
+//------------------------------------------------------------------------
+// Rôle de la classe <ASTGenerator>
+//
+//
+//
+//
+//------------------------------------------------------------------------
+
 class ASTGenerator : public ifccBaseVisitor {
+    //----------------------------------------------------------------- PUBLIC
+
   public:
-    virtual antlrcpp::Any visitAxiom(ifccParser::AxiomContext * ctx) override {
-        return visit(ctx->prog());
-    }
+    //----------------------------------------------------- Méthodes publiques
+    virtual antlrcpp::Any visitAxiom(ifccParser::AxiomContext * ctx) override;
 
-    virtual antlrcpp::Any visitProg(ifccParser::ProgContext * ctx) override {
-        program = new Program(ctx->start->getLine());
-        program->GetSymbolTable().DefineFunction("main", INT, ctx->start->getLine());
+    virtual antlrcpp::Any visitProg(ifccParser::ProgContext * ctx) override;
 
-        for (int i = 0; i < ctx->line().size(); i++) {
-            Instr * instr = (Instr *)visit(ctx->line(i));
-            if (instr != nullptr) {
-                program->AddInstr(instr);
-            }
-        }
+    virtual antlrcpp::Any visitLine(ifccParser::LineContext * ctx) override;
 
-        return program;
-    }
+    virtual antlrcpp::Any visitVar_decl(ifccParser::Var_declContext * ctx) override;
 
-    virtual antlrcpp::Any visitLine(ifccParser::LineContext * ctx) override {
-        Instr * instr;
-        if (ctx->var_aff()) {
-            instr = (Instr *)visit(ctx->var_aff());
-        } else if (ctx->return_stmt()) {
-            instr = (Instr *)visit(ctx->return_stmt());
-        } else if (ctx->var_decl()) {
-            instr = (Instr *)visit(ctx->var_decl());
-        }
+    virtual antlrcpp::Any visitVar_aff(ifccParser::Var_affContext * ctx) override;
 
-        return instr;
-    }
+    virtual antlrcpp::Any visitReturn_stmt(ifccParser::Return_stmtContext * ctx) override;
 
-    virtual antlrcpp::Any visitVar_decl(ifccParser::Var_declContext * ctx) override {
-        Instr * ret = nullptr;
-        if (program->GetSymbolTable().DefineVariable("main", ctx->VAR_NAME()->getText(), INT, ctx->start->getLine())) {
-            if (ctx->expr()) {
-                Expr * expr = (Expr *)visit(ctx->expr());
-                ret = new VarAffInstr(ctx->start->getLine(), ctx->VAR_NAME()->getText(), expr);
-            }
-        } else {
-            program->SetErrorFlag(true);
-        }
+    virtual antlrcpp::Any visitPar(ifccParser::ParContext * ctx) override;
 
-        return ret;
-    }
+    virtual antlrcpp::Any visitAdd(ifccParser::AddContext * ctx) override;
 
-    virtual antlrcpp::Any visitVar_aff(ifccParser::Var_affContext * ctx) override {
-        Expr * expr = (Expr *)visit(ctx->expr());
-        return (Instr *)new VarAffInstr(ctx->start->getLine(), ctx->VAR_NAME()->getText(), expr);
-    }
+    virtual antlrcpp::Any visitLess(ifccParser::LessContext * ctx) override;
 
-    virtual antlrcpp::Any visitReturn_stmt(ifccParser::Return_stmtContext * ctx) override {
-        Expr * expr = (Expr *)visit(ctx->expr());
-        return (Instr *)new ReturnInstr(ctx->start->getLine(), expr);
-    }
+    virtual antlrcpp::Any visitDiv(ifccParser::DivContext * ctx) override;
 
-    virtual antlrcpp::Any visitPar(ifccParser::ParContext * ctx) override {
-        return visit(ctx->expr());
-    }
+    virtual antlrcpp::Any visitMult(ifccParser::MultContext * ctx) override;
 
-    virtual antlrcpp::Any visitAdd(ifccParser::AddContext * ctx) override {
-        Expr * op1 = (Expr *)visit(ctx->expr(0));
-        Expr * op2 = (Expr *)visit(ctx->expr(1));
-        BinaryOperator binaryOperatorPlus = PLUS;
+    virtual antlrcpp::Any visitConst(ifccParser::ConstContext * ctx) override;
 
-        return (Expr *)new OpBin(ctx->start->getLine(), op1, op2, binaryOperatorPlus);
-    }
+    virtual antlrcpp::Any visitVar(ifccParser::VarContext * ctx) override;
 
-    virtual antlrcpp::Any visitLess(ifccParser::LessContext * ctx) override {
-        Expr * op1 = (Expr *)visit(ctx->expr(0));
-        Expr * op2 = (Expr *)visit(ctx->expr(1));
-        BinaryOperator binaryOperatorMinus = MINUS;
+    //-------------------------------------------- Constructeurs - destructeur
+    ASTGenerator() = default;
+    // Mode d'emploi :
+    //
+    // Contrat :
+    //
 
-        return (Expr *)new OpBin(ctx->start->getLine(), op1, op2, binaryOperatorMinus);
-    }
+    virtual ~ASTGenerator() override;
 
-    virtual antlrcpp::Any visitDiv(ifccParser::DivContext * ctx) override {
-        Expr * op1 = (Expr *)visit(ctx->expr(0));
-        Expr * op2 = (Expr *)visit(ctx->expr(1));
-        BinaryOperator binaryOperatorDiv = DIV;
-
-        return (Expr *)new OpBin(ctx->start->getLine(), op1, op2, binaryOperatorDiv);
-    }
-
-    virtual antlrcpp::Any visitMult(ifccParser::MultContext * ctx) override {
-        Expr * op1 = (Expr *)visit(ctx->expr(0));
-        Expr * op2 = (Expr *)visit(ctx->expr(1));
-        BinaryOperator binaryOperatorMult = MULT;
-
-        return (Expr *)new OpBin(ctx->start->getLine(), op1, op2, binaryOperatorMult);
-    }
-
-    virtual antlrcpp::Any visitConst(ifccParser::ConstContext * ctx) override {
-        return (Expr *)new ConstLiteral(ctx->start->getLine(), stoi(ctx->getText()));
-    }
-
-    virtual antlrcpp::Any visitVar(ifccParser::VarContext * ctx) override {
-        Expr * ret = nullptr;
-        if (program->GetSymbolTable().LookUp("main", ctx->VAR_NAME()->getText())) {
-            ret = new Var(ctx->start->getLine(), ctx->VAR_NAME()->getText());
-            program->GetSymbolTable().SetUsedVariable("main", ctx->VAR_NAME()->getText());
-        } else {
-            program->SetErrorFlag(true);
-            cerr << "variable " + ctx->VAR_NAME()->getText() + " does not exist in contextVariableTable from " + "main";
-        }
-
-        return ret;
-    }
+    //------------------------------------------------------------------ PRIVE
 
   protected:
+    //----------------------------------------------------- Attributs protégés
+
     Program * program;
 };
+//-------------------------------- Autres définitions dépendantes de <ASTGenerator>
+
+#endif // ASTGENERATOR_H
