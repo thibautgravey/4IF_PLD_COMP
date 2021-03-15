4043a0f7ca5281bac86a61cd0928fccda3010e7e
koechslin <killian.oechslin@insa-lyon.fr>
2021-03-15 10:41:14 +0100

    feat: remove forgotten useless methods

--- compiler/src/IR.cpp
+++ compiler/src/IR.cpp
@@ -46,18 +46,6 @@ void CFG::gen_asm_prologue(ostream & o) {
 void CFG::gen_asm_epilogue(ostream & o) {
 } //fin de gen_asm_epilogue
 
-void CFG::add_to_symbol_table(string name, Type t) {
-} //fin de add_to_symbole_table
-
-string CFG::create_new_tempvar(Type t) {
-} //fin de create_new_tempvar
-
-int CFG::get_var_index(string name) {
-} //fin de get_var_index
-
-Type CFG::get_var_type(string name) {
-} //fin de get_var_type
-
 string CFG::new_BB_name() {
 } //fin de new_BB_name
 
