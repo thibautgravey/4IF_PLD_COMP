ae070bed2b3f354554190cb03953cdf8cbb08bd7
koechslin <killian.oechslin@insa-lyon.fr>
2021-03-15 10:39:47 +0100

    feat: remove useless methods

--- compiler/src/IR.h
+++ compiler/src/IR.h
@@ -113,21 +113,12 @@ class CFG {
     void gen_asm_prologue(ostream & o);
     void gen_asm_epilogue(ostream & o);
 
-    // symbol table methods
-    void add_to_symbol_table(string name, Type t);
-    string create_new_tempvar(Type t);
-    int get_var_index(string name);
-    Type get_var_type(string name);
-
     // basic block management
     string new_BB_name();
     BasicBlock * current_bb;
 
   protected:
-    map<string, Type> SymbolType; /**< part of the symbol table  */
-    map<string, int> SymbolIndex; /**< part of the symbol table  */
-    int nextFreeSymbolIndex;      /**< to allocate new symbols in the symbol table */
-    int nextBBnumber;             /**< just for naming */
+    int nextBBnumber; /**< just for naming */
 
     vector<BasicBlock *> bbs; /**< all the basic blocks of this CFG*/
 };
