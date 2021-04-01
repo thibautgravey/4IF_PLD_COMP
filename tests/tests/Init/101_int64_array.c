1646a24d8b9a2561ecc928e5f6c1f848e45c90a2
Quentin de Andria <quentin.de.andria@gmail.com>
2021-04-01 21:28:50 +0200

    feat: new tests for arrays

---
+++ tests/tests/Init/101_int64_array.c
@@ -0,0 +1,16 @@
+#include <stdint.h>
+
+char main() {
+
+    int64_t a[4] = {74,65,87,1};
+    int64_t b[64];
+    int64_t c[] = {1234,3};
+
+    int64_t i = 32;
+
+    while (i = i - 1) {
+        b[i] = i-27;
+    }
+
+    return b[31]+a[c[1]];
+}
---
+++ tests/tests/Init/102_char_array.c
@@ -0,0 +1,17 @@
+#include <stdint.h>
+
+char main() {
+
+    char a[4] = {74,65,87,1};
+    char b[64];
+    char c[] = {1234,3};
+    char d[] = 'Hey!';
+
+    char i = 64;
+
+    while (i = i - 1) {
+        b[i] = i;
+    }
+
+    return b[1]+a[c[3]]+d[2];
+}
