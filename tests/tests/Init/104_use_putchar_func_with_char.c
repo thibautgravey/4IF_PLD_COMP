b56f0e461006639fe2f9ba04d4ab58f3f6436118
thibautgravey <thibaut.gravey@insa-lyon.fr>
2021-04-01 21:41:59 +0200

    test: putchar with char

---
+++ tests/tests/Init/104_use_putchar_func_with_char.c
@@ -0,0 +1,15 @@
+#include <stdint.h>
+
+int32_t main()
+{
+    putchar('H');
+    putchar('e');
+    putchar('x');
+    putchar('a');
+    putchar('O');
+    putchar('n');
+    putchar('e');
+    putchar(' ');
+    putchar('<');
+    putchar('3');
+}
